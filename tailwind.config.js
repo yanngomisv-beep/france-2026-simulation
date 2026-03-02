a1c636fd81198b38d0bcbb19c48883b6c5ed9c8f
yanngomisv-beep <yann.gomis.v@gmail.com>
2026-03-02 22:11:27 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,11 @@
-
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
